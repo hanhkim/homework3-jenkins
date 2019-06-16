1d1faa00b1d26a91eee2da21eb753341e6594012
Kim Hanh <hanhkim134@gmail.com>
2019-06-16 19:19:31 +0700

    change model

--- model/pet.js
+++ model/pet.js
@@ -1,5 +1,6 @@
 let ListData = [
     {id: 1, name: 'Kitty01', status: 'available'},
+    {id: 2, name: 'Kitty02', status: 'available'},
     {id: 4, name: 'Kitty04', status: 'available'},
     {id: 5, name: 'Kitty05', status: 'available'},
     {id: 6, name: 'Kitty06', status: 'available'},
